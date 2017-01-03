37b467a03676f9792717be4238c07ff483cf4696
Karl-Aksel Puulmann <karl@heapanalytics.com>
2017-01-03 16:16:36 +0200

    Queue test base.

---
+++ test/test-queue.ts
@@ -0,0 +1,70 @@
+import { expect } from "chai";
+import _ = require("lodash");
+import "mocha";
+
+import { ITaskCompletion, ProgressTracker, TaskQueue } from "../src";
+
+class FullProgressTracker extends ProgressTracker<string> {
+    enqueued: string[];
+    running: string[];
+    complete: Array<ITaskCompletion<string>>;
+
+    constructor() {
+        super();
+        this.enqueued = [];
+        this.running = [];
+        this.complete = [];
+    }
+
+    enqueueTask(task: string) {
+        super.enqueueTask(task);
+        this.enqueued.push(task);
+    }
+
+    startTask(task: string) {
+        super.startTask(task);
+        expect(this.enqueued).to.contain(task);
+
+        this.running.push(task);
+        _.remove(this.enqueued, task);
+    }
+
+    completeTask(result: ITaskCompletion<string>) {
+        super.completeTask(result);
+        expect(this.running).to.contain(result.task);
+        this.complete.push(result);
+        _.remove(this.running, result.task);
+    }
+}
+
+class TestQueue extends TaskQueue<string> {
+    tracker: FullProgressTracker;
+
+    constructor(keyParallelism: number, overallParallelism: number | null) {
+        super(
+            (task: string) => task.split(":")[0],
+            keyParallelism,
+            overallParallelism,
+            new FullProgressTracker(),
+        );
+    }
+
+    /* tslint:disable:no-empty */
+    executeTask(key: string, task: string): void {}
+    /* tslint:enable:no-empty */
+}
+
+describe("TaskQueue<TaskType>", () => {
+    let queue: TestQueue;
+
+    beforeEach(() => {
+        queue = new TestQueue(2, 4);
+    });
+
+    it("should immediately start tasks that were enqueued", () => {
+        queue.add("a:1");
+        queue.add("a:2");
+
+        expect(queue.tracker.enqueued).to.eql(["a:1", "a:2"]);
+    });
+});
