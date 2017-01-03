af30707a18874d939cd56e4148df1356ab3e7d61
Karl-Aksel Puulmann <karl@heapanalytics.com>
2017-01-03 17:14:47 +0200

    Add tests for testing overallParallelism, task finishing.

--- test/test-queue.ts
+++ test/test-queue.ts
@@ -26,14 +26,14 @@ class FullProgressTracker extends ProgressTracker<string> {
         expect(this.enqueued).to.contain(task);
 
         this.running.push(task);
-        this.enqueued = _.remove(this.enqueued, task);
+        this.enqueued = _.without(this.enqueued, task);
     }
 
     completeTask(result: ITaskCompletion<string>) {
         super.completeTask(result);
         expect(this.running).to.contain(result.task);
         this.complete.push(result);
-        this.running = _.remove(this.running, result.task);
+        this.running = _.without(this.running, result.task);
     }
 }
 
@@ -68,5 +68,37 @@ describe("TaskQueue<TaskType>", () => {
 
         expect(queue.tracker.enqueued).to.eql([]);
         expect(queue.tracker.running).to.eql(["a:1", "a:2"]);
+    });
+
+    it("should keep a queue for tasks that were over `keyParallelism`", () => {
+        queue.add("a:1");
+        queue.add("a:2");
+        queue.add("a:3");
+        queue.add("a:4");
+
+        expect(queue.tracker.running).to.eql(["a:1", "a:2"]);
+        expect(queue.tracker.enqueued).to.eql(["a:3", "a:4"]);
+    });
+
+    it("should keep up to `overallParallelism` tasks running", () => {
+        for (let letter of "abc") {
+            for (let index of [1, 2, 3]) {
+                queue.add(`${letter}:${index}`);
+            }
+        }
+
+        expect(queue.tracker.running).to.eql(["a:1", "a:2", "b:1", "b:2"]);
+        expect(queue.tracker.enqueued).to.have.lengthOf(5);
+    });
+
+    it("should start an enqueued task when another task finishes", () => {
+        queue.add("a:1");
+        queue.add("a:2");
+        queue.add("a:3");
+
+        expect(queue.tracker.running).to.eql(["a:1", "a:2"]);
+        queue.markTaskComplete("a:1");
+
+        expect(queue.tracker.running).to.eql(["a:2", "a:3"]);
     });
 });
